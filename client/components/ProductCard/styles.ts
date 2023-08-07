a03b920f4ec35aa95855e7aca80f26f1ebeb5b35
Виноградов Александр Сергеевич <ASeVinogradov@sberbank.ru>
2023-08-07 23:26:11 +0300

    client: fix ProductCard layout

--- client/components/ProductCard/styles.ts
+++ client/components/ProductCard/styles.ts
@@ -1,9 +1,9 @@
 export const s = {
-	cardWrapper: /*tw*/ 'relative w-[335px] py-5 px-2.5 shadow-[0px_1px_4px_rgba(99,99,99,0.15)] h-[613px]',
+	cardWrapper: /*tw*/ 'relative w-[335px] py-5 px-2.5 shadow-[0px_1px_4px_rgba(99,99,99,0.15)] h-[630px]',
 
-	nameBlock: /*tw*/ 'flex justify-between pb-2.5',
-	name: /*tw*/ 'text-[18px]  font-noto',
-	iconButton: /*tw*/ 'mr-2.5',
+	nameBlock: /*tw*/ 'flex justify-between items-start pb-2.5',
+	name: /*tw*/ 'text-[18px] leading-[22px] max-w-[270px] h-[44px] font-noto',
+	iconButton: /*tw*/ 'mr-2.5 mt-1.5',
 	code: /*tw*/ 'text-branded text-[14px]',
 
 	imgWrapper: /*tw*/ 'h-[289px] w-full flex flex-col justify-center items-center',
