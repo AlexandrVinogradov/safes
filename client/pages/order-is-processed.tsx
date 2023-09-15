6063b0628123145d2a9fac00cefb6dffb00c8c10
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2023-09-15 15:30:44 +0300

    client: page change

--- client/pages/order-is-processed.tsx
+++
@@ -1,8 +0,0 @@
-import { NextPage } from 'next'
-import OrderIsProcessedPage from '@/components/screens/OrderIsProcessedPage/OrderIsProcessedPage'
-
-const OrderIsProcessed: NextPage = () => {
-	return <OrderIsProcessedPage />
-}
-
-export default OrderIsProcessed
