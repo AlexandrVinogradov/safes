7bcecbf01ee1e7ac9e4684ffe3baf777851cfcdd
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2023-09-15 15:52:38 +0300

    client: _app remove ant

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,15 +1,15 @@
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
-import { ConfigProvider } from 'antd'
+// import { ConfigProvider } from 'antd'
 
 export default function App({ Component, pageProps }: AppProps) {
-	const theme = {
-		token: { colorPrimary: '#40A3D2' },
-	}
+	// const theme = {
+		// token: { colorPrimary: '#40A3D2' },
+	// }
 
 	return (
-		<ConfigProvider theme={theme}>
+		// <ConfigProvider theme={theme}>
 			<Component {...pageProps} />
-		</ConfigProvider>
+		// </ConfigProvider>
 	)
 }
