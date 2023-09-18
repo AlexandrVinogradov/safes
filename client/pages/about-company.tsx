8947688a97547afa2e294a8902828bc2a1a987ab
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2023-09-18 17:06:24 +0300

    _app (with tw) _documents contacts policies (content req)

--- client/pages/about-company.tsx
+++
@@ -1,8 +0,0 @@
-import { NextPage } from 'next'
-import AboutCompanyPage from '@/components/screens/AboutCompanyPage/AboutCompanyPage'
-
-const AboutCompany: NextPage = () => {
-	return <AboutCompanyPage />
-}
-
-export default AboutCompany
